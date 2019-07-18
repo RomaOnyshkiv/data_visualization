9a77d23cc4d38e205a3227d968d20bd3c19277e3
Roman Onyshkiv <roman_onyshkiv@epam.com>
2019-07-18 20:29:41 +0200

    cleaned unused code

--- visualize/urls.py
+++ visualize/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'results/(?P<year>\d+)/$$', views.results, name='results'),
+    url(r'results/(?P<year>\d+)/$', views.results, name='results'),
     url(r'^about/$', views.abut, name='about'),
 
 ]
